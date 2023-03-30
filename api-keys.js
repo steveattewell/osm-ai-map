7d6ae751f139beb69b49fde17b1d82853e73aaa1
steveattewell <steve.attewell@gmail.com>
2023-03-30 10:14:50 +0100

    Create api-keys.js
    Add your own API keys in here

---
+++ api-keys.js
@@ -0,0 +1,20 @@
+/*
+Insert your own API keys into this file.
+
+You can get an openAI API key from here: https://platform.openai.com/account/api-keys
+After you have signed up to OpenAI you will alos need to have access to GPT-4 - there is a waitlist for that as of  30 Mrch 2023
+You will need access to GPT-4 https://openai.com/waitlist/gpt-4-api
+You will need access to GPT-4 https://openai.com/waitlist/gpt-4-api
+https://openai.com/pricing
+
+You'll also need a maptiler API key from here: https://cloud.maptiler.com/account/keys/
+Pricing: https://www.maptiler.com/cloud/pricing/
+
+*/
+const keys = {
+  openAPIKey: "[INSERT_YOUR_OPENAI_API_KEY_HERE]", //https://platform.openai.com/account/api-keys
+  maptilerAPIKey: "[INSERT_YOUR_MAPTILER_API_KEY_HERE]" //https://cloud.maptiler.com/account/keys/
+};
+
+export default keys;
+
