5ea569b24f0e8d719ee68bcbdee62a0ae48e8211
steveattewell <steve.attewell@gmail.com>
2023-03-30 11:50:19 +0100

    Sync

--- api-keys.js
+++ api-keys.js
@@ -4,8 +4,7 @@ Insert your own API keys into this file.
 You can get an openAI API key from here: https://platform.openai.com/account/api-keys
 After you have signed up to OpenAI you will alos need to have access to GPT-4 - there is a waitlist for that as of  30 Mrch 2023
 You will need access to GPT-4 https://openai.com/waitlist/gpt-4-api
-You will need access to GPT-4 https://openai.com/waitlist/gpt-4-api
-https://openai.com/pricing
+Pricing: https://openai.com/pricing
 
 You'll also need a maptiler API key from here: https://cloud.maptiler.com/account/keys/
 Pricing: https://www.maptiler.com/cloud/pricing/
